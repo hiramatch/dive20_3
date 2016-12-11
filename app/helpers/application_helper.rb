e21701c13f912e88f43f2174f1e8bebb5b71662b
hirama <hiramatch@gmail.com>
2016-12-11 12:19:50 +0900

    トピック,ユーザー情報にプロフィール画像を表示

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
-  def profile_img(user)
-    # unless user.provider.blank?
-    #   img_url = user.image_url
-    # else
-    #   img_url = 'no_image.png'
-    # end
-    img_url = 'no_image.png'
+  def profile_img(user, size)
+   return image_tag(user.avatar, alt: user.name, size: size) if user.avatar?
 
+    unless user.provider.blank?
+      img_url = user.image_url
+    else
+      img_url = 'no_image.png'
+    end
     image_tag(img_url, alt: user.name)
   end
 end
