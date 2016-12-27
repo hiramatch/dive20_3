5b9caba1b44f609c023a2e9898c5914d01c9bfcc
hiramatch <hiramatch@gmail.com>
2016-12-28 01:49:53 +0900

    メール本文中のIPアドレスを修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: '52.199.118.119' }
+  config.action_mailer.default_url_options = { host: '52.199.88.235' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
    {
